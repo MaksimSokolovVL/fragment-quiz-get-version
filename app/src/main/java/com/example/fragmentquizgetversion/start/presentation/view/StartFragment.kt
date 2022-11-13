58f10ce7655bc005326e75bbc7ab76a7624b130b
MaksimSokolovVL <volokos2010@gmail.com>
2022-11-13 18:51:44 +0300

    version 0.0.3  -> del viewModel

--- app/src/main/java/com/example/fragmentquizgetversion/start/presentation/view/StartFragment.kt
+++ app/src/main/java/com/example/fragmentquizgetversion/start/presentation/view/StartFragment.kt
@@ -5,20 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.createViewModelLazy
-import androidx.lifecycle.ViewModel
 import com.example.fragmentquizgetversion.R
-
 import com.example.fragmentquizgetversion.databinding.StartFragmentBinding
 import com.example.fragmentquizgetversion.question.presentation.view.QuestionFragment
-import com.example.fragmentquizgetversion.start.presentation.viewmodel.StartViewModel
 
 class StartFragment : Fragment() {
 
     private var _binding: StartFragmentBinding? = null
     private val binding get() = _binding ?: throw RuntimeException("StartFragmentBinding == null")
 
-    private lateinit var viewModel: StartViewModel
 
     override fun onCreateView(  //из макета создать view
         inflater: LayoutInflater,
@@ -26,7 +21,6 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
         ): View {
         _binding = StartFragmentBinding.inflate(inflater)
-        viewModel = createViewModel()
         return binding.root
     }
 
@@ -36,12 +30,10 @@ class StartFragment : Fragment() {
     }
 
 
-    private fun createViewModel() = StartViewModel()
 
     private fun initView() {
         binding.buttonStart.setOnClickListener {
             launchQuestionFragment()
-//            findNavController().navigate(R.id.action_startFragment_to_questionFragment)
         }
     }
 
